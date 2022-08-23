b5cdc7a98d0509906c65f21abacb4f28250fa44b
Jesper van den Ende <jespertheend@gmail.com>
2022-08-24 00:56:52 +0200

    Support for providing an exact token in generateDeployToken.ts

--- generateDeployToken.ts
+++ generateDeployToken.ts
@@ -5,12 +5,18 @@ import { bytesToHex, digestString } from "./src/digestString.ts";
  * Usage: `deno run generateDeployToken.ts`.
  * The resulting token is what you use for authentication using the `DEPLOY_TOKEN` header.
  * The hash is what you place in a file that the `XXX_DEPLOY_TOKEN_PATH` environment variable points to.
+ * Alternatively you can provide the token as the first argument, for example:
+ * `deno run generateDeployToken.ts my_very_secure_password`
  */
 
-const values = new Uint8Array(32);
-crypto.getRandomValues(values);
+let token = Deno.args[0] || "";
 
-const token = bytesToHex(values);
+if (!token) {
+	const values = new Uint8Array(32);
+	crypto.getRandomValues(values);
+	token = bytesToHex(values);
+}
+
 const hash = await digestString(token);
 console.log(`TOKEN: ${token}
 HASH: ${hash}`);
